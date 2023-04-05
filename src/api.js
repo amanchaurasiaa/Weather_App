de426da0fb2a02210bcc7360c09111099cc549fe
amanchaurasiaa <aman.ch234@gmail.com>
2023-04-06 00:30:22 +0530

    First commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '117048c2e1msh6f680287483bc78p1fca84jsn7fc58eafd4e9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "9c346ae8b00b0e7fc0c48d8fc628e29b";
